ba1c40d09931000362db475d5ddd5d6ca5e08d38
Houda-Bakkour <93034967+Houda-Bakkour@users.noreply.github.com>
2021-10-24 15:27:46 +0200

    Update Program.cs

--- options/Program.cs
+++ options/Program.cs
@@ -1,71 +1,71 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace options
+namespace StableMarriage
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            var men = new List<Man>()
+            var men = new List<Etudiant>()
             {
-                new Man('A', new List<char>("IHJGF")),
-                new Man('B', new List<char>("FHJGI")),
-                new Man('C', new List<char>("HGJFI")),
-                new Man('D', new List<char>("HIFJG")),
-                new Man('E', new List<char>("JFHGI")),
+                new Etudiant('A', new List<char>("IHJGF"),13),
+                new Etudiant('B', new List<char>("FHJGI"),14),
+                new Etudiant('C', new List<char>("HGJFI"),12),
+                new Etudiant('D', new List<char>("HIFJG"),11),
+                new Etudiant('E', new List<char>("JFHGI"),16),
             };
-            var women = new List<Woman>()
+            var women = new List<Option>()
             {
-                new Woman('F', new List<char>("BECAD")),
-                new Woman('G', new List<char>("DECAB")),
-                new Woman('H', new List<char>("ECDAB")),
-                new Woman('I', new List<char>("BECDA")),
-                new Woman('J', new List<char>("DCBAE")),
+                new Option(1),
+                new Option(2),
+                new Option(1),
+
             };
 
-            var lonelyGuys = new List<Man>(men);
+            var SansOption = new List<Etudiant>();
 
-            // Carry on until there are no lonely guys anymore.
-            while (lonelyGuys.Count > 0)
+            
+            while (SansOption.Count > 0)
             {
-                // Just go trough the womens again and again.
-                for (int i = 0; i < women.Count; i++)
+                
+                for (int i = 0; i < Option.Count; i++)
                 {
                     // Create a list of the men, who want the women. 
-                    var bachelors = new List<Man>();
+                    var student = new List<Etudiant>();
                     // Add the lonely guys, who want to propose to the women.
-                    for (int j = 0; j < lonelyGuys.Count; j++)
+                    for (int j = 0; j < SansOption.Count; j++)
+
                     {
-                        if (lonelyGuys[j].NextChoiceId == women[i].Id)
+                        if (SansOption[j].NextChoices == Option[i].cap)
                         {
-                            bachelors.Add(lonelyGuys[j]);
-                            lonelyGuys[j].NextChoiceTried();
+                            student.Add(SansOption[j]);
+                            SansOption[j].NextChoiceTried();
                         }
                     }
 
-                    if (bachelors.Count > 0)
+                    if (student.Count > 0)
                     {
                         // Give the Husband a chance to prove himself again.
-                        if (women[i].Partner != null)
+                        if (Option[i].Partner != null)
                         {
-                            bachelors.Add(women[i].Partner);
-                            lonelyGuys.Add(women[i].Partner);
-                            women[i].Partner = null;
+                            student.Add(Option[i].Partner);
+                            SansOption.Add(Option[i].Partner);
+                            Option[i].Partner = null;
                         }
                         // Let the women choose.
-                        women[i].Partner = bachelors[0];
-                        for (int j = 1; j < bachelors.Count; j++)
+                        Option[i].Partner = student[0];
+                        for (int j = 1; j < student.Count; j++)
                         {
                             var keepPartner = true;
                             for (int k = 0; k < women[i].Choices.Count; k++)
                             {
-                                if (women[i].Choices[k] == women[i].Partner.Id)
-                                {
+                                if (Option[i].Choices[k] == Option[i] Partner.cap)
+                                
                                     keepPartner = true;
                                     break;
                                 }
-                                else if (women[i].Choices[k] == bachelors[j].Id)
+                                else if (Option[i].Choices[k] == student[j].Cne)
                                 {
                                     keepPartner = false;
                                     break;
@@ -73,18 +73,18 @@ namespace options
                             }
                             if (!keepPartner)
                             {
-                                women[i].Partner = bachelors[j];
+                                Option[i].Partner = student[j];
                             }
                         }
-                        lonelyGuys.Remove(women[i].Partner);
+                        SansOption.Remove(Option[i].Partner);
                     }
                 }
             }
 
             // Write the pairs to console
-            for (int i = 0; i < women.Count; i++)
+            for (int i = 0; i < Option.Count; i++)
             {
-                Console.WriteLine(women[i].Id + " : " + women[i].Partner.Id + " " + men.Count);
+                Console.WriteLine(Option[i].Id + " : " + Option[i].Partner.Id + " " +Etudiant.Count);
             }
         }
     }
@@ -93,48 +93,50 @@ namespace options
     {
         public List<char> Choices { get; set; }
         public char Cne { get; set; }
-
-        public Person(char cne, List<char> choices)
+        public int Note { get; set; }
+        public Person(char cne, List<char> choices, int note)
         {
-            Cne= cne;
+            Cne = cne;
             Choices = choices;
+            Note = note;
         }
     }
 
-    public class Woman : Person
+    public class Option
     {
-        public Etudiant  Partner { get; set; }
-
-        public Woman(char id, List<char> choices) : base(id, choices) { }
+        public Etudiant Partner { get; set; }
+        public Option (int cap);
+        
     }
+
 
     public class Etudiant : Person
     {
         public HashSet<char> ChoicesTried { get; set; } = new HashSet<char>();
-        public char? NextChoiceCne
+        public char? NextChoices
         {
             get
             {
-                char? choiceCne = null;
+                char? choices = null;
 
                 for (int i = 0; i < Choices.Count; i++)
                 {
                     if (!ChoicesTried.Contains(Choices[i]))
                     {
-                        choiceCne = Choices[i];
+                        choices = Choices[i];
                         break;
                     }
                 }
-                return choiceCne;
+                return choices;
             }
         }
-        public Woman Partner { get; set; }
+        public Option Partner { get; set; }
 
-        public Etudiant(char cne, List<char> choices) : base(cne, choices) { }
+        public Etudiant(char cne, List<char> choices,int note) : base( cne , choices,note) { }
 
         public void NextChoiceTried()
         {
-            ChoicesTried.Add((char)NextChoiceCne);
+            ChoicesTried.Add((char)NextChoices);
         }
     }
 }
